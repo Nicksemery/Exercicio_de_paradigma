6aa6691c34785641cb28c0aeaa48696855fddcf1
Nicolas <164393173+Nicksemery@users.noreply.github.com>
2026-03-20 14:30:51 -0300

    Update Lista_de_tarefas.py

--- Lista_de_tarefas/Lista_de_tarefas.py
+++ Lista_de_tarefas/Lista_de_tarefas.py
@@ -1,33 +1,5 @@
 lista = []
 
-def adicionando_item_na_lista():
-    nome = input("Digite o nome do item que deseja adicionar na lista")
-    return lista.append(nome)
-
-def removendo_item_da_lista():
-    nome = input("Digite o nome do item que deseja remover: ")
-    return lista.remove(nome)
-
-def mostrando_itens_da_lista():
-    for i in lista:
-        print(i)
-    
-def gerenciador_de_tarefas(valor):
-    match valor:
-        case 1:
-            print("Adicionar tarefa: ")
-            adicionando_item_na_lista()
-            print("Tarefa adicionado com sucesso!")
-        case 2:
-            print("Exibindo itens: ")
-            mostrando_itens_da_lista()
-        case 3:
-            print("Remover tarefa: ")
-            removendo_item_da_lista()
-            print("Tarefa removida com sucesso!")
-        case 0: 
-            print("Saindo...")
-
 while True:
     print("Bem vindo ao gerenciador de tarefas diarias!!")
     print("-------------------------------------------")
@@ -39,13 +11,19 @@ while True:
     
     while (teclado :=input("selecione uma opção: ")) not in {1,2,3,0}:
         if teclado == '1':
-            gerenciador_de_tarefas(1)
+           nome = input("Adicionar tarefa: ")
+           lista.append(nome)
+           print("Tarefa adicionado com sucesso!")
         elif teclado == '2':
-            gerenciador_de_tarefas(2)
+            print("Exibindo itens: ")
+            for i in lista:
+                print(i)
         elif teclado == '3':
-            gerenciador_de_tarefas(3)
+            nome = input("Remover tarefa: ")
+            lista.remove(nome)
+            print("Tarefa removida com sucesso!")
         else:
-            gerenciador_de_tarefas(0)
+            print("Saindo...")
             break
     while (resp := input('Deseja sair mesmo? [S/N] ').upper()) not in {'S', 'N'}:
         print('Valor INVÁLIDO!')
